6e12396c9de63528181e8c5c4f9b84a22b4c71a6
Docot Andonuts <gowingjm@gmail.com>
2016-10-25 16:34:35 -0400

    new fragment that almost works with back button

---
+++ app/src/main/java/com/doctorandonuts/trackthis/CreateTracker.java
@@ -0,0 +1,30 @@
+package com.doctorandonuts.trackthis;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CreateTracker extends Fragment {
+
+    public CreateTracker() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_tracker, container, false);
+    }
+}
--- app/src/main/java/com/doctorandonuts/trackthis/MainActivity.java
+++ app/src/main/java/com/doctorandonuts/trackthis/MainActivity.java
@@ -1,9 +1,8 @@
 package com.doctorandonuts.trackthis;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,24 +10,13 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-
-import com.couchbase.lite.CouchbaseLiteException;
-import com.couchbase.lite.Database;
-import com.couchbase.lite.Document;
-import com.couchbase.lite.Manager;
-import com.couchbase.lite.android.AndroidContext;
-
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,121 +29,143 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                CreateTracker createTracker = new CreateTracker();
+                // Sets the back stack to nothing, so when I back it will go back to main list screen.
+                getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.content_main, createTracker, "CreateTrackerFragment")
+                        .addToBackStack(null)
+                        .commit();
+                setDrawerState(false);
+                FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+                fab.hide();
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setTitle("Create New Tracker");
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
-        final String TAG = "HelloWorld";
-        Log.d(TAG, "Begin Hello World App");
-
-        // create a manager
-        Manager manager;
-        try {
-            manager = new Manager(new AndroidContext(this), Manager.DEFAULT_OPTIONS);
-            Log.d (TAG, "Manager created");
-        } catch (IOException e) {
-            Log.e(TAG, "Cannot create manager object");
-            return;
-        }
-
-        // create a name for the database and make sure the name is legal
-        String dbname = "hello";
-        if (!Manager.isValidDatabaseName(dbname)) {
-            Log.e(TAG, "Bad database name");
-            return;
-        }
-
-        // create a new database
-        Database database;
-        try {
-            database = manager.getDatabase(dbname);
-            Log.d (TAG, "Database created");
-
-        } catch (CouchbaseLiteException e) {
-            Log.e(TAG, "Cannot get database");
-            return;
-        }
-
-        // get the current date and time
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-        Calendar calendar = GregorianCalendar.getInstance();
-        String currentTimeString = dateFormatter.format(calendar.getTime());
-
-        // create an object that contains data for a document
-        Map<String, Object> docContent = new HashMap<String, Object>();
-        docContent.put("message", "Hello Couchbase Lite");
-        docContent.put("creationDate", currentTimeString);
-
-        // display the data for the new document
-        Log.d(TAG, "docContent=" + String.valueOf(docContent));
-
-        // create an empty document
-        Document document = database.createDocument();
-
-        // add content to document and write the document to the database
-        try {
-            document.putProperties(docContent);
-            Log.d (TAG, "Document written to database named " + dbname + " with ID = " + document.getId());
-        } catch (CouchbaseLiteException e) {
-            Log.e(TAG, "Cannot write document to database", e);
-        }
-
-        // save the ID of the new document
-        String docID = document.getId();
-
-        // retrieve the document from the database
-        Document retrievedDocument = database.getDocument(docID);
-
-        // display the retrieved document
-        Log.d(TAG, "retrievedDocument=" + String.valueOf(retrievedDocument.getProperties()));
-
-        // update the document
-        Map<String, Object> updatedProperties = new HashMap<>();
-        updatedProperties.putAll(retrievedDocument.getProperties());
-        updatedProperties.put("temperature", "95");
-
-        try {
-            retrievedDocument.putProperties(updatedProperties);
-            Log.d(TAG, "updated retrievedDocument=" + String.valueOf(retrievedDocument.getProperties()));
-        } catch (CouchbaseLiteException e) {
-            Log.e (TAG, "Cannot update document", e);
-        }
-
-        // delete the document
-        try {
-            retrievedDocument.delete();
-            Log.d (TAG, "Deleted document, deletion status = " + retrievedDocument.isDeleted());
-        } catch (CouchbaseLiteException e) {
-            Log.e (TAG, "Cannot delete document", e);
-        }
-
-        // retrieve the document from the database
-        Document retrievedDocumentDel = database.getDocument(docID);
-
-        // display the retrieved document
-        Log.d(TAG, "retrievedDocument=" + String.valueOf(retrievedDocumentDel.getProperties()));
-
-        Log.d(TAG, "End Hello World App");
+// THIS WAS JUST TEST CODE TO SHOW COUCH DATABASE WORKING
+//        final String TAG = "HelloWorld";
+//        Log.d(TAG, "Begin Hello World App");
+//
+//        // create a manager
+//        Manager manager;
+//        try {
+//            manager = new Manager(new AndroidContext(this), Manager.DEFAULT_OPTIONS);
+//            Log.d (TAG, "Manager created");
+//        } catch (IOException e) {
+//            Log.e(TAG, "Cannot create manager object");
+//            return;
+//        }
+//
+//        // create a name for the database and make sure the name is legal
+//        String dbname = "hello";
+//        if (!Manager.isValidDatabaseName(dbname)) {
+//            Log.e(TAG, "Bad database name");
+//            return;
+//        }
+//
+//        // create a new database
+//        Database database;
+//        try {
+//            database = manager.getDatabase(dbname);
+//            Log.d (TAG, "Database created");
+//
+//        } catch (CouchbaseLiteException e) {
+//            Log.e(TAG, "Cannot get database");
+//            return;
+//        }
+//
+//        // get the current date and time
+//        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+//        Calendar calendar = GregorianCalendar.getInstance();
+//        String currentTimeString = dateFormatter.format(calendar.getTime());
+//
+//        // create an object that contains data for a document
+//        Map<String, Object> docContent = new HashMap<String, Object>();
+//        docContent.put("message", "Hello Couchbase Lite");
+//        docContent.put("creationDate", currentTimeString);
+//
+//        // display the data for the new document
+//        Log.d(TAG, "docContent=" + String.valueOf(docContent));
+//
+//        // create an empty document
+//        Document document = database.createDocument();
+//
+//        // add content to document and write the document to the database
+//        try {
+//            document.putProperties(docContent);
+//            Log.d (TAG, "Document written to database named " + dbname + " with ID = " + document.getId());
+//        } catch (CouchbaseLiteException e) {
+//            Log.e(TAG, "Cannot write document to database", e);
+//        }
+//
+//        // save the ID of the new document
+//        String docID = document.getId();
+//
+//        // retrieve the document from the database
+//        Document retrievedDocument = database.getDocument(docID);
+//
+//        // display the retrieved document
+//        Log.d(TAG, "retrievedDocument=" + String.valueOf(retrievedDocument.getProperties()));
+//
+//        // update the document
+//        Map<String, Object> updatedProperties = new HashMap<>();
+//        updatedProperties.putAll(retrievedDocument.getProperties());
+//        updatedProperties.put("temperature", "95");
+//
+//        try {
+//            retrievedDocument.putProperties(updatedProperties);
+//            Log.d(TAG, "updated retrievedDocument=" + String.valueOf(retrievedDocument.getProperties()));
+//        } catch (CouchbaseLiteException e) {
+//            Log.e (TAG, "Cannot update document", e);
+//        }
+//
+//        // delete the document
+//        try {
+//            retrievedDocument.delete();
+//            Log.d (TAG, "Deleted document, deletion status = " + retrievedDocument.isDeleted());
+//        } catch (CouchbaseLiteException e) {
+//            Log.e (TAG, "Cannot delete document", e);
+//        }
+//
+//        // retrieve the document from the database
+//        Document retrievedDocumentDel = database.getDocument(docID);
+//
+//        // display the retrieved document
+//        Log.d(TAG, "retrievedDocument=" + String.valueOf(retrievedDocumentDel.getProperties()));
+//
+//        Log.d(TAG, "End Hello World App");
 
 
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        FragmentManager fragmentManager = getFragmentManager();
+        CreateTracker createTracker = (CreateTracker) fragmentManager.findFragmentByTag("CreateTrackerFragment");
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if (createTracker != null && createTracker.isVisible()) {
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.show();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setTitle(R.string.app_name);
+            setDrawerState(true);
+            super.onBackPressed();
         } else {
             super.onBackPressed();
         }
@@ -174,5 +184,19 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void setDrawerState(boolean isEnabled) {
+        if (isEnabled) {
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+            actionBarDrawerToggle.syncState();
+
+        }
+        else {
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            actionBarDrawerToggle.setDrawerIndicatorEnabled(false);
+            actionBarDrawerToggle.syncState();
+        }
     }
 }
